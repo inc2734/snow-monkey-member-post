c39eea473b7b4a19217d0d5a5328f80682d4bf95
Takashi Kitajima <inc@2inc.org>
2018-12-05 12:03:56 +0900

    1.1.2

--- snow-monkey-member-post.php
+++ snow-monkey-member-post.php
@@ -2,7 +2,7 @@
 /**
  * Plugin name: Snow Monkey Member Post
  * Description: It's a plugin that provides a function that allows only logged-in users to view articles.
- * Version: 1.1.1
+ * Version: 1.1.2
  *
  * @package snow-monkey-member-post
  * @author inc2734
