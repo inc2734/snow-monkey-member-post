b67cc844f77802e7fa0ff87ebf5ed1c70393038f
Takashi Kitajima <inc@2inc.org>
2019-08-20 16:31:53 +0900

    Update logout message design

--- templates/shortcode/logout/index.php
+++ templates/shortcode/logout/index.php
@@ -6,11 +6,6 @@
  */
 ?>
 
-<div class="c-row c-row--margin-s">
-	<div class="c-row__col c-row__col--1-1">
-		<?php esc_attr_e( 'Are you attempting to log out?', 'snow-monkey-member-post' ); ?>
-	</div>
-	<div class="c-row__col c-row__col--1-1">
-		<a href="<?php echo wp_logout_url( $redirect_to ); ?>" class="button logout-link"><?php esc_html_e( 'Log out', 'snow-monkey-member-post' ); ?></a>
-	</div>
+<div class="wpac-alert">
+	<?php esc_attr_e( 'Are you attempting to log out?', 'snow-monkey-member-post' ); ?> <a href="<?php echo wp_logout_url( $redirect_to ); ?>" class="button logout-link"><?php esc_html_e( 'Log out', 'snow-monkey-member-post' ); ?></a>
 </div>
