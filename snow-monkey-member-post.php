bfc5d250322a41bcdfcf5c54f3e295298404bb28
inc2734 <inc@2inc.org>
2024-11-13 08:09:55 +0900

    #61 load_plugin_textdomain() を init から後に変更

--- snow-monkey-member-post.php
+++ snow-monkey-member-post.php
@@ -34,9 +34,8 @@ class Bootstrap {
 	 * Plugins loaded.
 	 */
 	public function _bootstrap() {
-		load_plugin_textdomain( 'snow-monkey-member-post', false, basename( __DIR__ ) . '/languages' );
-
 		add_action( 'init', array( $this, '_add_attributes_to_blocks' ), 999999 );
+		add_action( 'init', array( $this, '_load_textdomain' ) );
 		add_action( 'init', array( $this, '_activate_autoupdate' ) );
 
 		$theme = wp_get_theme( get_template() );
@@ -119,6 +118,13 @@ class Bootstrap {
 				$block->attributes[ $name ] = $detail;
 			}
 		}
+	}
+
+	/**
+	 * Load textdomain.
+	 */
+	public function _load_textdomain() {
+		load_plugin_textdomain( 'snow-monkey-member-post', false, basename( __DIR__ ) . '/languages' );
 	}
 
 	/**
