e812fd547e08c3c0f249de6fbb936985cde58ada
inc2734 <inc@2inc.org>
2024-11-13 14:18:14 +0900

    6.2.4

--- snow-monkey-member-post.php
+++ snow-monkey-member-post.php
@@ -2,7 +2,7 @@
 /**
  * Plugin name: Snow Monkey Member Post
  * Description: It's a plugin that provides a function that allows only logged-in users to view articles.
- * Version: 6.2.3
+ * Version: 6.2.4
  * Tested up to: 6.7
  * Requires at least: 6.7
  * Requires PHP: 7.4
